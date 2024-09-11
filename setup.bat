3e3b08dcbae02631a12a7ea04ffa363926045b08
Parallaxes <lucasmasterson@gmail.com>
2024-09-10 20:25:46 -0700

    Update

--- setup.bat
+++ setup.bat
@@ -2,15 +2,21 @@
 REM Function to install dependencies
 :install_dependencies
 echo Installing dependencies...
-pip install -r requirements.txt
+pip install --user -r requirements.txt
 goto :eof
 
 REM Function to add krayt to PATH
 :add_to_path
 echo Adding krayt to system PATH...
 set "SCRIPT_DIR=%~dp0"
-setx PATH "%PATH%;%SCRIPT_DIR%"
-echo krayt has been added to your PATH. You can now use the 'krayt' command.
+REM Check if the PATH already contains the SCRIPT_DIR
+echo %PATH% | find /i "%SCRIPT_DIR%" >nul
+if %errorlevel%==0 (
+    echo The script directory is already in the PATH.
+) else (
+    setx PATH "%PATH%;%SCRIPT_DIR%"
+    echo krayt has been added to your PATH. You can now use the 'krayt' command.
+)
 goto :eof
 
 REM Install dependencies
